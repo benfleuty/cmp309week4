2c04d6cfbe027ee3560b4aa98d7cd23a8cfa5270
Ben Fleuty <bfleuty@outlook.com>
2022-03-06 02:43:07 +0000

    Get incoming call info

--- TelephonySMS/app/src/main/java/uk/ac/abertay/telephonysms/CallReceiver.java
+++ TelephonySMS/app/src/main/java/uk/ac/abertay/telephonysms/CallReceiver.java
@@ -9,14 +9,29 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class CallReceiver extends BroadcastReceiver {
+
+    final String TAG = "BroadcastReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
-            String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-
-            final String TAG = "PhoneStateListener";
-            Log.i(TAG,"Call state is now " + state);
+            actionPhoneStateChanged(intent);
+        } else if (action.equals(Intent.ACTION_NEW_OUTGOING_CALL)){
+            actionNewOutgoingCall(intent);
         }
+    }
+
+    private void actionNewOutgoingCall(Intent intent) {
+        String oldNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+        Log.i(TAG, "To: " + oldNumber);
+        setResultData("0123456789");
+    }
+
+    private void actionPhoneStateChanged(Intent intent) {
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+        final String TAG = "PhoneStateListener";
+        Log.i(TAG,"Call state is now " + state + " on number " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
     }
 }
