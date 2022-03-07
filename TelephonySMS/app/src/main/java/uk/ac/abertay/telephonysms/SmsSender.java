987f9892132ab86266e633a8b9c11b435d204437
Ben Fleuty <bfleuty@outlook.com>
2022-03-07 05:34:09 +0000

    Add empty activity to use for sending SMS

---
+++ TelephonySMS/app/src/main/java/uk/ac/abertay/telephonysms/SmsSender.java
@@ -0,0 +1,14 @@
+package uk.ac.abertay.telephonysms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmsSender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_sender);
+    }
+}
