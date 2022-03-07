ed4e7d4be48def28a5927d174de04b0f502e016e
Ben Fleuty <bfleuty@outlook.com>
2022-03-07 05:54:29 +0000

    Add SMS Sender layout
    * Import some layout from week 2
    * Add empty click events

--- TelephonySMS/app/src/main/java/uk/ac/abertay/telephonysms/SmsSender.java
+++ TelephonySMS/app/src/main/java/uk/ac/abertay/telephonysms/SmsSender.java
@@ -3,6 +3,7 @@ package uk.ac.abertay.telephonysms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SmsSender extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class SmsSender extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_sender);
+    }
+
+    public void btnSelectContact_onClick(View view) {
+    }
+
+    public void btnSendSms_onClick(View view) {
     }
 }
